7de03bd1467646bdbfa5d1c71c377aa3816aaf48
Felipe Pedroso da Silva <pedroso.asti@gmail.com>
2015-06-09 12:35:15 -0300

    Alteração de texto do botão

--- app/src/main/java/br/com/felipepedroso/secondapp/contas/FragmentContas.java
+++ app/src/main/java/br/com/felipepedroso/secondapp/contas/FragmentContas.java
@@ -13,7 +13,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
